c31a53f249e377dc1b11ab7f324d131f470817ce
ohminyoung <minyoung_stat@dgu.ac.kr>
2022-12-07 22:14:47 +0900

    textStyle added

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -6,12 +6,68 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.orange, Colors.white],
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _Logo(),
+              _AppName(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Image.asset(
+        "asset/image/logo.png",
+      ),
+    );
+  }
+}
+
+class _AppName extends StatelessWidget {
+  const _AppName({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      color: Colors.black,
+      fontSize: 30.0,
+      fontWeight: FontWeight.w300,
+    );
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 128.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset("asset/image/logo.png"),
-          Row(
-            children: [Text("VIDEO"), Text('Player')],
-          )
+          Text(
+            "VIDEO",
+            style: textStyle,
+          ),
+          Text(
+            'Player',
+            style: textStyle.copyWith(
+              fontWeight: FontWeight.w700,
+            ),
+          ),
         ],
       ),
     );
